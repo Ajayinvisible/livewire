5507a90e1df3d16091b501f09669510902621fb8
Ajay Rai <rai.invisibleweb@gmail.com>
2025-06-18 23:43:57 +0545

    feat: larvel form validation

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -3,31 +3,40 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Counter extends Component
 {
-    public $name;
-    public $email;
-    public $password;
     public function render()
     {
         $users = User::all();
-        return view('livewire.counter',[
+        return view('livewire.counter', [
             'users' => $users
         ]);
     }
 
+    // validation rule
+    #[Rule('required|string|min:3|max:100')]
+    public $name;
+
+    #[Rule('required|min:5|email|max:255|unique:users')]
+    public $email;
+
+    #[Rule('required|min:5')]
+    public $password;
+
     public function createNewUser()
     {
+        $validated = $this->validate();
         User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => $this->password,
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => $validated['password'],
         ]);
 
         // rest input fields
-        $this->reset(['name','email','password']);
+        $this->reset(['name', 'email', 'password']);
         // flash message
         request()->session()->flash('success', 'user created successfully!');
     }
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -8,12 +8,21 @@
     <form wire:submit='createNewUser' action="">
         <div class="mb-3">
             <input wire:model='name' type="text" name="name" id="name" placeholder="name">
+            @error('name')
+                <small class="text-danger d-block">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <input wire:model='email' type="email" name="email" id="email" placeholder="email">
+            @error('email')
+                <small class="text-danger d-block">{{ $message }}</small>
+            @enderror
         </div>
         <div class="mb-3">
             <input wire:model='password' type="password" name="password" id="password" placeholder="password">
+            @error('password')
+                <small class="text-danger d-block">{{ $message }}</small>
+            @enderror
         </div>
         <button class="btn btn-success">Create</button>
         {{-- prevent default --}}
@@ -22,7 +31,7 @@
     <hr>
     <ul>
         @foreach ($users as $user)
-            <li>{{ $user->name }}</li>
+            <li wire:key='{{ $user->id }}'>{{ $user->name }}</li>
         @endforeach
     </ul>
 </div>
